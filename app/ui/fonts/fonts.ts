6b5ebf5b90194dd48c668de192fa97389a14df9e
thomas <thomas.graber310@gmail.com>
2023-11-28 09:44:41 +0100

    feat(font): add projects font

---
+++ app/ui/fonts/fonts.ts
@@ -0,0 +1,8 @@
+import {IBM_Plex_Sans} from "next/font/google";
+import localFont from "next/font/local"
+
+export const ibm_plex_sans400 = IBM_Plex_Sans({subsets: ['latin'], weight: ['400']})
+export const ibm_plex_sans500 = IBM_Plex_Sans({subsets: ['latin'], weight: ['500']})
+export const ibm_plex_sans700 = IBM_Plex_Sans({subsets: ['latin'], weight: ['700']})
+
+export const druk700 =({src: './fonts/DrukText-Bold-Trial.otf'})
