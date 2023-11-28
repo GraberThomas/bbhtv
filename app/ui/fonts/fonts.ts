9728ce111eb167ac1548147347e8d04718876d97
thomas <thomas.graber310@gmail.com>
2023-11-28 10:49:41 +0100

    feat(font): move font in app folder

--- app/ui/fonts/fonts.ts
+++ app/ui/fonts/fonts.ts
@@ -1,9 +1,9 @@
 import localFont from "next/font/local";
 
-export const druk_bold = localFont({src: './fonts/Druk/DrukText-Bold.otf'})
+export const druk_bold = localFont({src: '../../fonts/Druk/druk_text/DrukText-Bold.otf'})
 
-export const ibm_plex_sans_bold = localFont({src: './fonts/IBM_Plex_Sans/IBMPlexSans-Bold.ttf'})
+export const ibm_plex_sans_bold = localFont({src: '../../fonts/IBM_Plex_Sans/IBMPlexSans-Bold.ttf'})
 
-export const ibm_plex_sans_medium = localFont({src: './fonts/IBM_Plex_Sans/IBMPlexSans-Medium.ttf'})
+export const ibm_plex_sans_medium = localFont({src: '../../fonts/IBM_Plex_Sans/IBMPlexSans-Medium.ttf'})
 
-export const ibm_plex_sans_regular = localFont({src: './fonts/IBM_Plex_Sans/IBMPlexSans-Regular.ttf'})
+export const ibm_plex_sans_regular = localFont({src: '../../fonts/IBM_Plex_Sans/IBMPlexSans-Regular.ttf'})
