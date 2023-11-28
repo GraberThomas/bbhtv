7423e0083c02bac4475539875accf1dd45ba867c
thomas <thomas.graber310@gmail.com>
2023-11-28 09:58:54 +0100

    feat(fonts): add fonts declaration

--- app/ui/fonts/fonts.ts
+++ app/ui/fonts/fonts.ts
@@ -1,8 +1,9 @@
-import {IBM_Plex_Sans} from "next/font/google";
-import localFont from "next/font/local"
+import localFont from "next/font/local";
 
-export const ibm_plex_sans400 = IBM_Plex_Sans({subsets: ['latin'], weight: ['400']})
-export const ibm_plex_sans500 = IBM_Plex_Sans({subsets: ['latin'], weight: ['500']})
-export const ibm_plex_sans700 = IBM_Plex_Sans({subsets: ['latin'], weight: ['700']})
+export const druk_bold = localFont({src: './fonts/Druk/DrukText-Bold.otf'})
 
-export const druk700 =({src: './fonts/DrukText-Bold-Trial.otf'})
+export const ibm_plex_sans_bold = localFont({src: './fonts/IBM_Plex_Sans/IBMPlexSans-Bold.ttf'})
+
+export const ibm_plex_sans_medium = localFont({src: './fonts/IBM_Plex_Sans/IBMPlexSans-Medium.ttf'})
+
+export const ibm_plex_sans_regular = localFont({src: './fonts/IBM_Plex_Sans/IBMPlexSans-Regular.ttf'})
